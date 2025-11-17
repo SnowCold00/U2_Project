af677bc739b0baf244667e85f3b78490e30b9d95
olcol <olcold00@gmail.com>
2025-11-17 18:46:51 -0500

    worked

--- src/Stock.java
+++ src/Stock.java
@@ -1,10 +1,28 @@
+import java.text.DecimalFormat;
+
 public class Stock {
 
     private String name;
     private double price;
+    DecimalFormat format1 = new DecimalFormat("###,###,##0.00");
 
     public Stock(String name, double price){
         this.name = name;
+        this.price = price;
+    }
+
+    public String priceChange(){
+        int percentChange = (int) ((Math.random()*250)+1);
+        price *= percentChange/100.0;
+        return format1.format(price);
+    }
+
+    public double sell(){
+        return price;
+    }
+
+    public void info(){
+        System.out.println(name + " $" + format1.format(price));
     }
 
 }
